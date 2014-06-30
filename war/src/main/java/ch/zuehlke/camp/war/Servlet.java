eb21c1880d7cc7fb3dbab9d5835c28e024fcaff2
Martin <x@email.com>
2014-06-30 18:18:37 +0200

    Added Servlet.

---
+++ war/src/main/java/ch/zuehlke/camp/war/Servlet.java
@@ -0,0 +1,29 @@
+package ch.zuehlke.camp.war;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/TestServlet"})
+public class Servlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Test 1</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<p>Test</p>");
+        out.println("</body>");
+    }
+}
