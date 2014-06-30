0fbd784fcdd58394b4ef2f58c1d6ce49f4a3a177
marc <hae@zuehlke.com>
2014-06-30 15:58:15 +0200

    Added EchoBean

--- ejb/src/main/java/ch/zuehlke/camp/ejb/EchoBean.java
+++ ejb/src/main/java/ch/zuehlke/camp/ejb/EchoBean.java
@@ -1,7 +1,15 @@
 package ch.zuehlke.camp.ejb;
 
-//@Stateless
-//@Remote(EchoBeanRemote.class)
-public class EchoBean {
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+
+@Stateless
+@Remote(EchoBeanRemote.class)
+public class EchoBean implements EchoBeanRemote {
+
+	@Override
+	public String echo(String echoText) {
+		return "Echo " + echoText;
+	}
 
 }
