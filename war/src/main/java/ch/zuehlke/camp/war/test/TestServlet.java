4ef21dc438d177c3c062d9358cca1c3d03036615
Martin <x@email.com>
2014-07-01 10:59:44 +0200

    cosmetics

--- war/src/main/java/ch/zuehlke/camp/war/test/TestServlet.java
+++ war/src/main/java/ch/zuehlke/camp/war/test/TestServlet.java
@@ -13,19 +13,16 @@ import javax.servlet.http.HttpServletResponse;
 public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = -9089231358752688259L;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    }
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-
         PrintWriter out = response.getWriter();
         out.println("<html>");
         out.println("<head>");
-        out.println("<title>Test 1</title>");
+        out.println("<title>Test-Servlet</title>");
         out.println("</head>");
         out.println("<body>");
         out.println("<p>Test-Servlet</p>");
         out.println("</body>");
     }
+
 }
