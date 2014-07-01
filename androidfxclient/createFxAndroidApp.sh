1a2b658be11b610da52503a03ee1e6b00f0e89a5
ccuz <ccu@zuehlke.com>
2014-07-01 15:56:55 +0200

    Add minimal JavaFx App

--- androidfxclient/createFxAndroidApp.sh
+++ androidfxclient/createFxAndroidApp.sh
@@ -5,4 +5,4 @@ export PATH=$ANDROID_SDK/tools:$PATH
 export OPENJFX_DIR=~/dalvik-sdk
 export WORKDIR=~/git/camp
 export PROJECT_DIR=~/git/camp/javafxclient/dist
-gradle --info createProject -PDEBUG -PDIR=$WORKDIR -PPACKAGE=zuehlke.food -PNAME=androidfxclient -PANDROID_SDK=$ANDROID_SDK -PJFX_SDK=$OPENJFX_DIR -PJFX_APP=$PROJECT_DIR -PJFX_MAIN=zuehlke.food.HelloWorld
+gradle --info createProject -PDEBUG -PDIR=$WORKDIR -PPACKAGE=zuehlke.food -PNAME=androidfxclient -PANDROID_SDK=$ANDROID_SDK -PJFX_SDK=$OPENJFX_DIR -PJFX_APP=$PROJECT_DIR -PJFX_MAIN=zuehlke.food.ZenFoodApp
