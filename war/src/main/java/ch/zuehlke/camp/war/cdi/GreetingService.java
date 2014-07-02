b2b2392d2b137e8888bfa9443af76738b5236f25
Martin <x@email.com>
2014-07-02 11:43:25 +0200

    CDI: Using own annotation.

--- war/src/main/java/ch/zuehlke/camp/war/cdi/GreetingService.java
+++ war/src/main/java/ch/zuehlke/camp/war/cdi/GreetingService.java
@@ -10,7 +10,8 @@ import javax.inject.Inject;
 public class GreetingService implements Serializable {
 
 	private static final long serialVersionUID = -5805097361121982143L;
-	@Inject SimpleGreeting greeting;
+//	@Inject Greeting greeting;
+	@Inject @Fancy Greeting greeting;
 
 	public String greet(String name) {
 		return greeting.greet(name);
