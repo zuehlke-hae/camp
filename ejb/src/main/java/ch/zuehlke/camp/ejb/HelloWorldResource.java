108198795f520c120903d31c3510e5c52491b439
Florian Besser <flbe@zuehlke.com>
2014-07-02 16:25:12 +0200

    Removed unnecessary stuff, fixed batch

--- ejb/src/main/java/ch/zuehlke/camp/ejb/HelloWorldResource.java
+++ ejb/src/main/java/ch/zuehlke/camp/ejb/HelloWorldResource.java
@@ -11,8 +11,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Path;
 
-import ch.zuehlke.camp.jpa.Person;
-
 // The Java class will be hosted at the URI path "/helloworld"
 @Path("/helloworld")
 @PersistenceContext(name = "ExampleDS")
@@ -33,42 +31,6 @@ public class HelloWorldResource {
 	@Produces("text/plain;charset=" + encoding)
 	public String getClichedMessage() {
 		
-		JobOperator jo = BatchRuntime.getJobOperator();
-		long jid = jo.start("myJob", new Properties());
-
-		
-		Person person = new Person();
-		person.setFirstName("Hans");
-		person.setLastName("Müller");
-		
-		em.persist(person);
-		
-		//Now look it up again (for test)
-		Person result = em.find(Person.class, person.getId());
-		
-		return "Created " + person.toString() + ", found " + result;
-//		ContactBla test = em.find(ContactBla.class, 1L);
-		
-		
-		
-//		if (test == null) {
-//			test = new ContactBla("Hello", " World");
-//
-//
-//			// At this Point the Entity does not have a
-//			// Persistent Identity and is not associated
-//			// with a persistent Context
-//			em.persist(test); // Persist the Entity
-////			em.flush();
-//			// At this point the Entity has a Persistent
-//			// Identity and is associated with a Persistent
-//			// context.
-//			
-//
-//			return "New contact created.";
-//		} else {
-//			return "Contact found: " + test.getFirstName() + " "
-//					+ test.getLastName();
-//		}
+		return "hello world";
 	}
 }
