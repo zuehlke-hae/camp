34ca4895b5a470316edf7429c775310ff97fb2d1
Florian Besser <flbe@zuehlke.com>
2014-07-02 10:13:07 +0200

    Added batch stuff, for reading data to the DB

--- ejb/src/main/java/ch/zuehlke/camp/ejb/HelloWorldResource.java
+++ ejb/src/main/java/ch/zuehlke/camp/ejb/HelloWorldResource.java
@@ -1,5 +1,9 @@
 package ch.zuehlke.camp.ejb;
 
+import java.util.Properties;
+
+import javax.batch.operations.JobOperator;
+import javax.batch.runtime.BatchRuntime;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
@@ -26,6 +30,9 @@ public class HelloWorldResource {
 	// type "text/plain"
 	@Produces("text/plain")
 	public String getClichedMessage() {
+		
+		JobOperator jo = BatchRuntime.getJobOperator();
+		long jid = jo.start("myJob", new Properties());
 
 		
 		Person person = new Person();
